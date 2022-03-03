4ca22caed01c2e646ebcb4419cca5d9d450c7579
Thomas Houghton <tomhoughton888@gmail.com>
2022-03-03 15:11:39 +0000

    Modified Server and added new datasets

--- server/server.py
+++ server/server.py
@@ -1,23 +1,42 @@
-import os
-import pandas as pd 
-import numpy
 from flask import Flask
+import pandas as pd
+import os
+import numpy as np
 import json
 
-
+# Init:
 app = Flask(__name__)
+temp_path = os.path.join('temp')
+df = pd.read_csv(os.path.join(temp_path, 'Mar-03-2022.csv'))
 
-@app.route("/api/get_table")
-def rtn_table():
-    data = pd.read_csv('Salary_Data.csv')
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/api/get-data')
+def get_data():
     
+    # Columns:
+    columns = df.columns
+    
+    # Data Dictionary:
     data_dict = {}
-    data_dict['colums'] = data.columns.tolist()
+    
+    for col in columns:
+        new_column = df[col].tolist()
+        data_dict[col] = new_column
+        
+        
+    data_dict['ColumnNames'] = columns.tolist()
+    
+   
+    
+   
+    
+    
+    return {'data': data_dict }
+    
 
-    for col in data.columns:
-        data_dict[col] = data[col].to_list()
-
-    return { 'data': data_dict }
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
+    
